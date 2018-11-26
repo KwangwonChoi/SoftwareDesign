251166ebf3066e5ba754bbf6d99e10bcae512b73
Kwangwon Choi <rhkddnjs99@hotmail.com>
2018-11-27 05:54:23 +0900

    addJSON_Login

---
+++ JavaProjectFile/ExchangeStudentManage/src/MemberInfoManage/LoginInfo.java
@@ -0,0 +1,13 @@
+package MemberInfoManage;
+
+public class LoginInfo {
+
+	public String _id;
+	public String _pw;
+	
+	public LoginInfo(String id, String pw) {
+		_id = id;
+		_pw = pw;
+	}
+	 
+}
---
+++ JavaProjectFile/ExchangeStudentManage/src/MemberInfoManage/MemberInfo.java
@@ -0,0 +1,5 @@
+package MemberInfoManage;
+
+public class MemberInfo {
+
+}
