38f710974cfd61e763c1a6707b919dce72663137
nsun9505 <nsun9505@daum.net>
2018-12-04 20:48:00 +0900

    Program&Application Valid Check Complete

--- JavaProjectFile/ExchangeStudentManage/src/Posts/Application.java
+++ JavaProjectFile/ExchangeStudentManage/src/Posts/Application.java
@@ -2,6 +2,7 @@ package Posts;
 
 import java.io.File;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import DataManage.JsonFormat.ApplicationInfo;
 import Member.Student;
@@ -9,12 +10,12 @@ import Member.Student;
 public class Application {
 	 
 	private Program _program;
-	private String _programName;
-	private Student _student;
+	private String _programName;			// valid check error number : 1
+	private Student _student;			
 	private APPLICATIONSTATE _state;
-	private float _score;
+	private float _score;					// valid check error number : 2
 	private Object _studyPlan;
-	private float _langGrade;
+	private float _langGrade;				// valid check error number : 3
 	
 	public Application(Program program, APPLICATIONSTATE state, float score, Object studyPlan, float langGrade) {
 		this._program = program;
@@ -61,6 +62,57 @@ public class Application {
 		return aInfo;
 	}
 	
+	public static int isValidName(String name) {
+		name = name.replaceAll(" ", "");
+		if(name == null || name.equals("")) {
+			System.err.println("ÀÌ¸§À» ÀÔ·ÂÇØ ÁÖ¼¼¿ä.");
+			return 1;
+		}
+		
+		if(Pattern.matches("^[a-zA-Z°¡-ÆR]*$", name))
+			return 0;
+		
+		if(name.matches("^[0-9]*$")) {
+			System.err.println("ÀÌ¸§¿¡´Â ¼ýÀÚ°¡ Æ÷ÇÔµÉ ¼ö ¾ø½À´Ï´Ù.");
+			return 1;
+		}
+		else {
+			System.err.println("ÀÌ¸§¿¡´Â Æ¯¼ö¹®ÀÚ¸¦ Æ÷ÇÔÇÒ ¼ö ¾ø½À´Ï´Ù.");
+			return 1;
+		}
+	}
+	
+	public static int isValidScore(String score) {
+		float g;
+		score = score.replaceAll(" ", "");
+		try {
+			g = Float.parseFloat(score);
+		}catch(NumberFormatException e) {
+			System.err.println("¼ýÀÚ¸¦ ÀÔ·ÂÇØÁÖ¼¼¿ä.");
+			return 2;
+		}catch(NullPointerException e) {
+			System.err.println("Á¡¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä.");
+			return 2;
+		}
+		return 0;
+	}
+	
+	public static int isValidLangGrade(String langGrade) {
+		float g;
+		langGrade = langGrade.replaceAll(" ", "");
+		try {
+			g = Float.parseFloat(langGrade);
+		}catch(NumberFormatException e) {
+			System.err.println("¼ýÀÚ¸¦ ÀÔ·ÂÇÏ¼¼¿ä.");
+			return 3;
+			
+		}catch(NullPointerException e) {
+			System.err.println("¾îÇÐ ¼ºÀûÀ» ÀÔ·ÂÇÏ¼¼¿ä.");
+			return 3;
+		}
+		return 0;
+	}
+	
 	public void SetStudent(Student s) {
 		this._student = s;
 	}
@@ -92,5 +144,4 @@ public class Application {
 	public final String get_programName() {
 		return _programName;
 	}
-
 }
